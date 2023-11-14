a13e14b4388fb46db3d2643c81dfcf93ddfeaa87
kuwachi626 <s1asdfghjkl@yahoo.co.jp>
2023-11-15 01:27:05 +0900

    古墳公園AR追加

--- link.js
+++
@@ -1,5 +0,0 @@
-$(window).on('load', function() {
-    if  (navigator.userAgent.match(/iPhone|iPad.+Mobile|Macintosh/) && 'ontouchend' in document) {
-        $('.test_link').attr('href','./usdz/townhall.usdz')
-    }
-});
